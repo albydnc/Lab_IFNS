32c63e8aa90c0c03780f25ecc01e0f37ee73f31e
Daniele Pani <36479584+danielepani999@users.noreply.github.com>
2019-02-25 19:14:58 +0100

    Add files via upload

---
+++ sipm/sigma33.C
@@ -0,0 +1,74 @@
+  #include <iostream>           // ci serve per stampare a video il testo
+#include <TGraphErrors.h>     // ci serve per istanziare grafici
+#include <TAxis.h>            // ci serve per manipolare gli assi dei grafici
+#include <TCanvas.h>          // ci serve per disegnare i grafici
+#include <TLegend.h>
+#include <TF1.h>              // ci serve per scrivere le funzioni con cui fittare i grafici
+#include <iomanip>            // ci serve per manipolare l'output a video
+using namespace std;
+
+
+void sigma33(){
+
+int nmisure = 8;
+float n[8];
+float sigma[] = {19.5889531204, 25.1942333486, 28.8259360599, 32.4067798227, 37.7147713333, 39.471538553, 46.4557342567, 53.3905598967};
+float sn[8];
+float ssigma[] = {5.4146001826, 5.1815149687, 5.0947719164, 5.0353303437, 4.9756841479, 4.9609271182, 4.9176277994, 4.8901258067};
+for (int j=0;j<8;j++){
+n[j]=pow(j+1,0.5);
+sn[j]=0.25/n[j];
+}
+
+
+
+
+
+
+  // --------------------- Grafico i(V) ------------------------------ //
+  // Creo il canvas (la tela) su cui disegnare il grafico. "ciV" è il il suo nome in memoria
+  // è bene che sia unico dentro al programma. "i(v)" è semplicemente il titolo del canvas
+  // le due coppie di numeri che seguono sono le coordinate dell'angolo superiore sinistro e le seconde
+  // la dimensione in pixel lungo x e lungo y
+  TCanvas *ciV = new TCanvas("ciV","Poisson",200,10,600,400);
+  // Mi assicuro che la tela sia bianca (0 corrisponde al bianco, per altri colori vedi https://root.cern.ch/doc/master/classTColor.html)
+  ciV->SetFillColor(0);
+  // Mi assicuro che disegni su questa tela e non su un altra. Ciò è particolarmente importante quando ho tanti canvas aperti.
+  ciV->cd();
+  // Istanzio il grafico. Il costruttore che noi usiamo prende come argomenti:
+  // il numero di misure da disegnare, l'array di misure x (=V), l'array di misure y (=i), e gli
+  // array delle  rispettive incertezze
+  TGraphErrors *giV = new TGraphErrors(nmisure,n,sigma,sn,ssigma);
+  // Nelle due righe successive disegno i punti del grafico con lo stile che più mi piace.
+  // Vedi anche https://root.cern.ch/doc/master/classTAttMarker.html
+  giV->SetMarkerSize(0.6);
+  giV->SetMarkerStyle(21);
+  // Facile, titolo del grafico
+  giV->SetTitle("i(V)");
+  // Titoli degli assi
+  giV->GetXaxis()->SetTitle("V [V]");
+  giV->GetYaxis()->SetTitle("i [A]");
+  // Do istruzioni al grafico di disegnarsi sul canvas che ho selezionato preventivamente con cd()
+  // Esistono diverse opzioni di disegno, vedi anche https://root.cern.ch/doc/master/classTGraphPainter.html
+  // "AP" è molto semplice, gli stiamo chiedendo di disegnare gli assi (A) e i punti (P)
+  giV->Draw("AP");
+
+  cout << "\n\n --- Fit I(V) pow([0],1/([1]+1))*pow(x,([1]-1)/([1]+1)) \n" <<endl;
+  TF1 *funz0 = new TF1("funz0","[0]+[1]*x",0,8);
+  // cambio colore alla linea della funzione in modo da distinguerla dalla polinomiale di ordine 4 quando la andrò a disegnare
+  funz0->SetLineStyle(1); // Blu
+  funz0->SetLineColor(14);
+  // Istruzione per fare il fit ai dati usando la funzione funz1 sopra definita
+  // + significa che voglio aggiungere la funzione funz1 alla lista delle funzioni già disegnate sul canvas
+  // R significa che voglio fare il fit solo in un determinato range, definito quando ho creato funz1. Quindi in questo caso (0-130) Ohm
+  // In questo caso, mettere o non mettere l'opzione R è equivalente, visto che 0-130 copre tutto il range delle misure.
+  // M dice di usare Hesse che fa una stima più accurata delle incertezze sui parametri. Nel caso di problemi rimuovere l'opzione.
+  funz0->SetParameter(0,0);
+  //funz0->SetParameter(1,9);
+  giV->Fit(funz0,"RM+");
+
+
+  cout << "Chi^2:" << funz0->GetChisquare() << ", number of DoF: " << funz0->GetNDF() << " (Probability: " << funz0->GetProb() << ")." << endl;
+  cout << "--------------------------------------------------------------------------------------------------------" << endl;
+
+}
